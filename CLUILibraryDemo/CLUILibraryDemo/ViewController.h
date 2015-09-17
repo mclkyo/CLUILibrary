5008ef37041f1da87ae916fcb375ebb31e275bee
mclkyo <247105654@qq.com>
2015-09-17 15:01:13 +0800

    just a few class.to test cocoaspod

---
+++ CLUILibraryDemo/CLUILibraryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLUILibraryDemo
+//
+//  Created by yilecity on 15/9/17.
+//  Copyright (c) 2015年 yilecity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Libs/UIView+PositionExtension.h
@@ -0,0 +1,20 @@
+//
+//  UIView+PositionExtension.h
+//  SocketClient
+//
+//  Created by yilecity on 15/9/9.
+//  Copyright (c) 2015年 yilecity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView(PositionExtension)
+
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+
+@end
